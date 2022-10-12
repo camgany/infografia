76744809fdd2943033da94c8761239e274ec5682
camigrandy <camgany@gmail.com>
2022-10-12 19:03:01 -0400

    last

--- state_machine/scenes/characters/Enemy.gd
+++ state_machine/scenes/characters/Enemy.gd
@@ -1,12 +1,14 @@
 extends KinematicBody2D
 
-export(int) var w_range = 50
+export(int) var w_range = 100
 
 var speed = 200
-var accel = 2
+var accel = 10
 var velocity = Vector2.ZERO
 onready var state_machine = $AnimationTree.get("parameters/playback")
-
+var hurt = false
+var die = false
+var hp = 5
 onready var start_position = global_position
 onready var target_position = global_position
 
@@ -29,10 +31,25 @@ func _physics_process(delta):
 		state_machine.travel("idle")
 		velocity = Vector2.ZERO
 	move_and_slide(velocity)
+	if hurt:
+		state_machine.travel("hurt")
+		hurt = false
+	
+	if die:
+		state_machine.travel("dead")
+		velocity = Vector2.ZERO
+		#hurt = false
 
 func _on_Timer_timeout():
 	print("mover")
 	update_target_position()
-	var duration = rand_range(10, 16)
+	var duration = rand_range(200, 200)
 	timer.start(duration)
+	
 
+func _on_HurtBox_area_entered(area):
+	hurt = true
+	hp -= 1
+	if hp <= 0:
+		die = true
+	print(area)
